7dd9fa670cc8473bfc5a7ad47543d6b3dac282b8
jacostaf10 <jorge.acosta.freire@gmail.com>
2023-04-16 11:27:28 +0200

    Initial Commit

---
+++ LayeredCells/LayeredCellsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LayeredCellsApp.swift
+//  LayeredCells
+//
+//  Created by Jorge Acosta Freire on 16/4/23.
+//
+
+import SwiftUI
+
+@main
+struct LayeredCellsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
